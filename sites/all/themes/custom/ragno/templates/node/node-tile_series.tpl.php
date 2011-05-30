2ee610914d5fdedcfe01183bfdc1dc8a2b14d358
premanandad <premanandad@gmail.com>
2011-05-30 12:59:24 +0530

    Modified to change the tech spec Label

--- sites/all/themes/custom/ragno/templates/node/node-tile_series.tpl.php
+++ sites/all/themes/custom/ragno/templates/node/node-tile_series.tpl.php
@@ -24,16 +24,18 @@
   		<div class="field field-type-text field-field-sizes">
   			<div class="field-items"><?php print $sizes_list; ?></div>
   		</div>
-  	<?php endif;?>
+  	<?php endif;?>	
   	<?php if (!empty($node->field_tech_spec)): ?>
+	
   		<div class="field field-type-filefield field-field-tech-spec">
-  			<div class="field-label"><?php print_r ($node->content['field_tech_spec']['field']['#title']); ?></div>
-  			<div class="field-items"><div class="filefield-item"><div><a href="<?php print base_path() . $node->field_tech_spec[0]['filepath']; ?>" target="_blank">Click Here to Download</a></div></div></div>
+  			<div class="field-label"><?php // print_r ($node->content['field_tech_spec']['field']['#title']); ?>Series Overview</div>
+  			<div class="field-items"><div class="filefield-item"><div><a href="<?php print base_path() . $node->field_tech_spec[0]['filepath']; ?>" target="_blank">Printer Friendly Version</a></div></div></div>
   		</div>
   	<?php endif;?>   
   	<div class="field field-type-filefield">
   		<div class="field-label">catalog</div>
-                <div class="field-items"><div class="filefield-item"><a href="<?php print base_path() . drupal_get_path_alias('node/' . $node->field_related_catalog[0]['nid']) ?>">View The Product Catalog Online</a></div></div>
+		
+		<div class="field-items"><div class="filefield-item"><a href="<?php print base_path() . drupal_get_path_alias('node/' . $node->field_related_catalog[0]['nid']) ?>">View The Product Catalog Online</a></div></div>
   	</div> 	
   	<div class="field field-type-filefield">
   		<div class="field-label">CARE &amp; MAINTENANCE</div>
