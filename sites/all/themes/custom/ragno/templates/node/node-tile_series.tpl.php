5e467f25d98562d8ebbfd107b7457ddd9ae02b42
premanandad <premanandad@gmail.com>
2011-05-31 19:47:26 +0530

    modified to fix the pd page opening up directly

--- sites/all/themes/custom/ragno/templates/node/node-tile_series.tpl.php
+++ sites/all/themes/custom/ragno/templates/node/node-tile_series.tpl.php
@@ -25,8 +25,9 @@
   			<div class="field-items"><?php print $sizes_list; ?></div>
   		</div>
   	<?php endif;?>	
-  	<?php if (!empty($node->field_tech_spec)): ?>
-	
+  	<?php if (!empty($node->field_tech_spec)): 
+			$nodes = node_load($node->field_related_catalog[0]['nid']);
+	?>
   		<div class="field field-type-filefield field-field-tech-spec">
   			<div class="field-label"><?php // print_r ($node->content['field_tech_spec']['field']['#title']); ?>Series Overview</div>
   			<div class="field-items"><div class="filefield-item"><div><a href="<?php print base_path() . $node->field_tech_spec[0]['filepath']; ?>" target="_blank">Printer Friendly Version</a></div></div></div>
@@ -35,7 +36,7 @@
   	<div class="field field-type-filefield">
   		<div class="field-label">catalog</div>
 		
-		<div class="field-items"><div class="filefield-item"><a href="<?php print base_path() . drupal_get_path_alias('node/' . $node->field_related_catalog[0]['nid']) ?>">View The Product Catalog Online</a></div></div>
+		<div class="field-items"><div class="filefield-item"><a href="<?php print base_path() . $nodes->field_catalog_file[0]['filepath']; ?>" target="_blank">View The Product Catalog Online</a></div></div>
   	</div> 	
   	<div class="field field-type-filefield">
   		<div class="field-label">CARE &amp; MAINTENANCE</div>
